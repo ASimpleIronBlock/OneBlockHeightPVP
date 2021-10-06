18604d566640a8eb132a0a56e732317978b9338a
Iron__Block <62505063+ASimpleIronBlock@users.noreply.github.com>
2021-10-06 18:47:28 +0800

    可以配置资源生成概率了

---
+++ src/main/java/me/ironblock/oneblockpvp/oneblockpvpplugin/event/functions/RejoinFunction.kt
@@ -0,0 +1,10 @@
+package me.ironblock.oneblockpvp.oneblockpvpplugin.event.functions
+
+import me.ironblock.oneblockpvp.oneblockpvpplugin.utils.ITickable
+import org.bukkit.event.Listener
+
+object RejoinFunction:Listener, ITickable {
+    override fun tick() {
+        TODO("Not yet implemented")
+    }
+}
---
+++ src/main/java/me/ironblock/oneblockpvp/oneblockpvpplugin/utils/ITickable.kt
@@ -0,0 +1,5 @@
+package me.ironblock.oneblockpvp.oneblockpvpplugin.utils
+
+interface ITickable {
+    fun tick()
+}
